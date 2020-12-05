6e324a05e0b069e6c9ebe7dc99291ea75fda80f0
jannkasper <jann.kasper@gmail.com>
2020-12-05 09:03:54 +0100

    react-hooks reducer

--- react-hooks/src/components/TemperatureInput.js
+++ react-hooks/src/components/TemperatureInput.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useEffect, useReducer} from "react";
 import scaleNames from "../constants/ScaleName";
+
+function init(initialTemperature) {
+    return {temperature: initialTemperature};
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return {temperature: parseFloat(state.temperature) ? parseFloat(state.temperature) + 1 : 1};
+        case 'decrement':
+            return {temperature: parseFloat(state.temperature) ? parseFloat(state.temperature) - 1 : -1};
+        case 'value':
+            return {temperature: parseFloat(action.value) ? parseFloat(action.value) : action.value};
+        default:
+            throw new Error();
+    }
+}
 
 function TemperatureInput(props) {
 
+    const [state, dispatch] = useReducer(reducer, props.temperature, init);
+
+    useEffect(() => {
+        props.onTemperatureChange(props.scale, state.temperature);
+    }, [state.temperature]);
+
     function handleChange(e) {
-        props.onTemperatureChange(props.scale, e.target.value);
+        dispatch({type: "value", value: e.target.value})
     }
 
     return (
         <div>
             <p>Enter temperature in {scaleNames[props.scale]}: </p>
             <input value={props.temperature} onChange={handleChange}/>
+            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+            <button onClick={() => dispatch({type: 'increment'})}>+</button>
         </div>
     )
 }
